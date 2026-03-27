f7e5994c9e9c6cd4f10517b0dba04483a340a8ae
Shreyas Rode <shreyasrode2503@gmail.com>
2026-03-27 10:56:04 +0530

    Add comments to SQL queries for clarity
    Added comments to clarify SQL queries for revenue, orders, and average purchase value.

--- SQL Queries/Basic_Analysis.sql
+++ SQL Queries/Basic_Analysis.sql
@@ -1,11 +1,14 @@
--- What is the total revenue generated?
+-- Q1: What is the total revenue generated?
+-- Total Revenue
 SELECT SUM(purchase_amount_usd) AS total_revenue
 FROM orders;
 
--- How many total orders were placed?
+-- Q2: How many total orders were placed?
+-- Total Orders
 SELECT COUNT(*) AS total_orders
 FROM orders;
 
--- What is the average purchase value?
+-- Q3: What is the average purchase value?
+-- Avg Purchase Value
 SELECT AVG(purchase_amount_usd) AS avg_purchase_value
 FROM orders;
